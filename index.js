8fac29b44d68134486c7d4a0429ff8e9c2ae9610
Dan Shevenell <dan.shev@gmail.com>
2017-12-05 17:18:49 -0500

    quick polish

--- index.js
+++ index.js
@@ -68,6 +68,12 @@ function OccupiPlatform(log, config, api) {
   }
 }
 
+// Method to restore accessories from cache
+cmdSwitchPlatform.prototype.configureAccessory = function (accessory) {
+  this.setService(accessory)
+  this.accessories[accessory.context.room_id] = accessory
+}
+
 
 // Method to setup accesories from config.json
 OccupiPlatform.prototype.didFinishLaunching = function () {
@@ -204,13 +210,13 @@ OccupiPlatform.prototype.setService = function(accessory) {
 
 
 OccupiPlatform.prototype.getOccupancyDetectorOccupancyStatus = function (accessory, callback) {
-  callback()
+  callback(null, accessory.state)
 }
 
 
 OccupiPlatform.prototype.setOccupancyDetectorOccupancyStatus = function (accessory, toState, callback) {
-  this.context.state = toState
-  callback()
+  accessory.state = toState
+  callback(null)
 }
 
 
@@ -226,9 +232,6 @@ OccupiPlatform.prototype.getInitState = function (accessory) {
     .setCharacteristic(Characteristic.Manufacturer, manufacturer)
     .setCharacteristic(Characteristic.Model, model)
     .setCharacteristic(Characteristic.SerialNumber, serial)
-
-
-  // Perhaps query database and init occupancy state based on routine?
 
   Room.getByID(accessory.context.room_id, (success, roomData) => {
     if(!success) {
@@ -272,7 +275,6 @@ OccupiPlatform.prototype.getInitState = function (accessory) {
 
         })
       } else {
-          
           accessory.getService(Service.OccupancySensor)
             .setCharacteristic(Characteristic.OccupancyDetected, true)
       }
@@ -289,375 +291,7 @@ OccupiPlatform.prototype.getInitState = function (accessory) {
 
 
 
-
-
-// Function invoked when homebridge tries to restore cached accessory.
-// Developer can configure accessory at here (like setup event handler).
-// Update current value.
-OccupiPlatform.prototype.configureAccessory = function(accessory) {
-  this.log(accessory.displayName, "Configure Accessory")
-  platform = this
-
-  // Set the accessory to reachable if plugin can currently process the accessory,
-  // otherwise set to false and update the reachability later by invoking 
-  // accessory.updateReachability()
-  accessory.reachable = true
-
-  accessory.on('identify', function(paired, callback) {
-    platform.log(accessory.displayName, "Identify!!!")
-    callback()
-  })
-
-  if (accessory.getService(Service.Lightbulb)) {
-    accessory.getService(Service.Lightbulb)
-    .getCharacteristic(Characteristic.On)
-    .on('set', function(value, callback) {
-      platform.log(accessory.displayName, "Light -> " + value)
-      callback()
-    })
-  }
-
-  this.accessories.push(accessory)
-}
-
-
-
-// Handler will be invoked when user try to config your plugin.
-// Callback can be cached and invoke when necessary.
-OccupiPlatform.prototype.configurationRequestHandler = function(context, request, callback) {
-  this.log("Context: ", JSON.stringify(context))
-  this.log("Request: ", JSON.stringify(request))
-
-  // Check the request response
-  if (request && request.response && request.response.inputs && request.response.inputs.name) {
-    this.addAccessory(request.response.inputs.name)
-
-    // Invoke callback with config will let homebridge save the new config into config.json
-    // Callback = function(response, type, replace, config)
-    // set "type" to platform if the plugin is trying to modify platforms section
-    // set "replace" to true will let homebridge replace existing config in config.json
-    // "config" is the data platform trying to save
-    callback(null, "platform", true, {"platform":"OccupiPlatform", "otherConfig":"SomeData"})
-    return
-  }
-
-  // - UI Type: Input
-  // Can be used to request input from user
-  // User response can be retrieved from request.response.inputs next time
-  // when configurationRequestHandler being invoked
-
-  respDict = {
-    "type": "Interface",
-    "interface": "input",
-    "title": "Add Accessory",
-    "items": [
-      {
-        "id": "name",
-        "title": "Name",
-        "placeholder": "Fancy Light"
-      }//, 
-      // {
-      //   "id": "pw",
-      //   "title": "Password",
-      //   "secure": true
-      // }
-    ]
-  }
-
-  // - UI Type: List
-  // Can be used to ask user to select something from the list
-  // User response can be retrieved from request.response.selections next time
-  // when configurationRequestHandler being invoked
-
-  // respDict = {
-  //   "type": "Interface",
-  //   "interface": "list",
-  //   "title": "Select Something",
-  //   "allowMultipleSelection": true,
-  //   "items": [
-  //     "A","B","C"
-  //   ]
-  // }
-
-  // - UI Type: Instruction
-  // Can be used to ask user to do something (other than text input)
-  // Hero image is base64 encoded image data. Not really sure the maximum length HomeKit allows.
-
-  // respDict = {
-  //   "type": "Interface",
-  //   "interface": "instruction",
-  //   "title": "Almost There",
-  //   "detail": "Please press the button on the bridge to finish the setup.",
-  //   "heroImage": "base64 image data",
-  //   "showActivityIndicator": true,
-  // "showNextButton": true,
-  // "buttonText": "Login in browser",
-  // "actionURL": "https://google.com"
-  // }
-
-  // Plugin can set context to allow it track setup process
-  context.ts = "Hello"
-
-  // Invoke callback to update setup UI
-  callback(respDict)
-}
-
-
-
-
-
-OccupiPlatform.prototype.updateAccessoriesReachability = function() {
-  this.log("Update Reachability")
-  for (index in this.accessories) {
-    accessory = this.accessories[index]
-    accessory.updateReachability(false)
-  }
-}
-
-// Sample function to show how developer can remove accessory dynamically from outside event
-OccupiPlatform.prototype.removeAccessory = function() {
-  this.log("Remove Accessory")
-  this.api.unregisterPlatformAccessories("homebridge-occupi", "SamplePlatform", this.accessories)
-
-  this.accessories = []
-}
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-const Service, Characteristic
- 
-module.exports = function (homebridge) {
-  Service = homebridge.hap.Service
-  Characteristic = homebridge.hap.Characteristic
-  homebridge.registerAccessory("switch-plugin", "MyAwesomeSwitch", mySwitch)
-}
-
-mySwitch.prototype = {
-  getServices: function () {
-    let informationService = new Service.AccessoryInformation()
-    informationService
-      .setCharacteristic(Characteristic.Manufacturer, "Shevenell")
-      .setCharacteristic(Characteristic.Model, "v0.1")
-      .setCharacteristic(Characteristic.SerialNumber, "123-456-789")
-
-    let switchService = new Service.Switch("My switch")
-    switchService
-      .getCharacteristic(Characteristic.On)
-        .on('get', this.getSwitchOnCharacteristic.bind(this))
-        .on('set', this.setSwitchOnCharacteristic.bind(this))
- 
-    this.informationService = informationService
-    this.switchService = switchService
-    return [informationService, switchService]
-  }
-}
-
-const request = require('request')
-const url = require('url')
- 
-function mySwitch(log, config) {
-  this.log = log
-  this.getUrl = url.parse(config['getUrl'])
-  this.postUrl = url.parse(config['postUrl'])
-}
- 
-
-mySwitch.prototype = {
- 
-  getSwitchOnCharacteristic: function (next) {
-    const me = this
-    request({
-        url: me.getUrl,
-        method: 'GET',
-    }, 
-    function (error, response, body) {
-      if (error) {
-        me.log('STATUS: ' + response.statusCode)
-        me.log(error.message)
-        return next(error)
-      }
-      return next(null, body.currentState)
-    })
-  },
-   
-  setSwitchOnCharacteristic: function (on, next) {
-    const me = this
-    request({
-      url: me.postUrl,
-      body: {'targetState': on},
-      method: 'POST',
-      headers: {'Content-type': 'application/json'}
-    },
-    function (error, response) {
-      if (error) {
-        me.log('STATUS: ' + response.statusCode)
-        me.log(error.message)
-        return next(error)
-      }
-      return next()
-    })
-  }
-}
-
-
-
-
-
-
-
-
-
-const Service, Characteristic
- 
-module.exports = function (homebridge) {
-  Service = homebridge.hap.Service
-  Characteristic = homebridge.hap.Characteristic
-  homebridge.registerAccessory("homebridge-occupi", "OccupiSensor", OccupiSensor)
-}
-
-function OccupiSensor(log, config) {
-  this.log = log
-  this.getUrl = url.parse(config['getUrl'])
-  this.postUrl = url.parse(config['postUrl'])
-}
-
-
-const request = require('request')
-const url = require('url')
- 
-OccupiSensor.prototype = {
- 
-  getOccupancyStatus: function (next) {
-    const me = this
-    request({
-        url: me.getUrl,
-        method: 'GET',
-    }, 
-    function (error, response, body) {
-      if (error) {
-        me.log('STATUS: ' + response.statusCode)
-        me.log(error.message)
-        return next(error)
-      }
-      return next(null, body.currentState)
-    })
-  },
-   
-  setOccupancyStatus: function (occupied, next) {
-    const me = this
-    request({
-      url: me.postUrl,
-      body: {'targetState': occupied},
-      method: 'POST',
-      headers: {'Content-type': 'application/json'}
-    },
-    function (error, response) {
-      if (error) {
-        me.log('STATUS: ' + response.statusCode)
-        me.log(error.message)
-        return next(error)
-      }
-      return next()
-    })
-  }
-}
-
-OccupiSensor.prototype = {
-  getServices: function () {
-    let informationService = new Service.AccessoryInformation()
-    informationService
-      .setCharacteristic(Characteristic.Manufacturer, "Shevenell")
-      .setCharacteristic(Characteristic.Model, "v0.1")
-      .setCharacteristic(Characteristic.SerialNumber, "123-456-789")
-
-    // Required characteristic
-    let occupancyService = new Service.OccupancySensor("Occupi Sensor")
-    occupancyService
-      .getCharacteristic(Characteristic.OccupancyDetected)
-        .on('get', this.getOccupancyStatus.bind(this))
-        .on('set', this.setOccupancyStatus.bind(this))
- 
-    this.informationService = informationService
-    this.occupancyService = occupancyService
-    return [informationService, occupancyService]
-  }
-}
-
-
-/* Add to config.json in Homebridge diretory:
-
-
-{
-    "bridge": {
-        "name": "Homebridge",
-        "username": "CC:22:3D:E3:CE:30",
-        "port": 51826,
-        "pin": "031-45-154"
-    },
-    
-    "description": "This is an example configuration file with one fake accessory and one fake platform. You can use this as a template for creating your own configuration file containing devices you actually own.",
-
-    "accessories": [
-        {
-            "accessory": "WeMo",
-            "name": "Coffee Maker"
-        }
-    ],
-
-    "platforms": [
-        {
-            "platform" : "Shevenell-Occupancy-Kit",
-            "name" : "OccupancyKit"
-        }
-    ]
-}
-
-
-
-
-
-
-
-{
-  "accessory": "myOccupancySensor1",
-  "getUrl": "http://192.168.0.10/api/status",
-  "postUrl": "http://192.168.0.10/api/order"
-}
-
-
-
-
-
-
-
-
-
-
+/*
 
 
 const bluetooth = require('node-bluetooth')
